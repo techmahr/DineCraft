a777d7a1af18ba428db665d0dcdc0d360633a50f
Mahroof <mahroof@Mac.lan>
2025-12-25 14:50:46 +0530

    initial animation added

--- assets/js/app.js
+++ assets/js/app.js
@@ -51,3 +51,12 @@ if (spinnerWrapperEl) {
         }, 400); 
     });
 }
+
+// Initialize AOS
+AOS.init({
+  // Global settings:
+  duration: 800, // values from 0 to 3000, with step 50ms
+  easing: 'ease-in-out', // default easing for AOS animations
+  once: true, // whether animation should happen only once - while scrolling down
+  mirror: false, // whether elements should animate out while scrolling past them
+});
